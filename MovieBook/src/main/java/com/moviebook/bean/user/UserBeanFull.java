2554398eaa363c810d2f6c3d2d8627c388dec332
Slimcea <slimcea@gmail.com>
2017-04-07 05:35:53 +0800

    Refactoring for user bean

---
+++ MovieBook/src/main/java/com/moviebook/bean/user/UserBeanFull.java
@@ -0,0 +1,13 @@
+package com.moviebook.bean.user;
+
+public interface UserBeanFull {
+
+	public int getId();
+
+	public String getEmail();
+
+	public String getName();
+
+	public String getProfilePhotoPath();
+
+}
---
+++ MovieBook/src/main/java/com/moviebook/bean/user/UserBeanLight.java
@@ -0,0 +1,11 @@
+package com.moviebook.bean.user;
+
+public interface UserBeanLight {
+
+	public int getId();
+
+	public String getName();
+
+	public String getProfilePhotoPath();
+
+}
